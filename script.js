a313a42e417fe2849e6927883f1811ecd9ccf7e2
Dios <Dios@pi.edu.co>
2025-09-02 08:45:58 -0500

    explicacion de js

---
+++ script.js
@@ -0,0 +1,22 @@
+let numero1
+let numero2
+
+numero1 = parseInt (prompt("Digite su numero1"))
+numero2 = parseInt (prompt("Digite su numero2"))
+
+
+if(numero1 > numero2){
+ alert ("el mayor es el: " + numero1)
+
+
+
+}else if (numero1 === numero2) {
+     alert ("los numeros son iguales: " + numero2)
+
+}else{
+    alert ("el mayor es el: " + numero2)
+}
+
+
+
+
