c34d19370b5126210fcb110cc95796b02797daa5
fabian sanabria <hfabiansanabria_15@hotmail.com>
2025-09-02 08:54:12 -0500

    hice un comig

---
+++ script.js
@@ -0,0 +1,18 @@
+let numero1
+let numero2
+numero1=parseInt (prompt("cual es tu numero"))
+numero2=parseInt (prompt( "cual es tu numero"))
+
+if (numero1> numero2)
+{
+    alert("el numero mayor es:" + numero1)
+    
+
+    }else if(numero1 === numero2)
+    {  
+        alert("los numeros son iguales")
+
+} else{
+    alert("el numero mayor es:" + numero2)
+    
+}
