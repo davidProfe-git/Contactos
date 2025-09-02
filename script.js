fc6fbbdf22a4b3480c0b830bfd270b48e9215af4
David Rugeles <david.rugeles@pi.edu.co>
2025-09-02 08:51:07 -0500

    sintaxis basica js

---
+++ script.js
@@ -0,0 +1,40 @@
+//sintaxis basica
+
+//tipos de datos nativos
+let texto = "texto"
+let entero = 5 //number
+let decimal = 5.4 //number
+let objeto = {nombre: "david"}
+let indefinido
+let nulo = null
+let booleano = true || false
+
+
+// console.log(typeof(texto))
+// console.log(typeof(entero))
+// console.log(typeof(decimal))
+
+//variables y constantes
+let numero1 
+let numero2
+numero1 = parseInt(prompt("¿dame un numero 1?"))
+numero2 = parseInt(prompt("¿dame un numero 2?"))
+
+
+
+//estructuras de control
+ if(numero1 > numero2)
+    {
+        alert("el numero mayor es: "+ numero1)
+ }
+ else if(numero1 === numero2)
+    {
+    alert("son iguales")
+ }
+ else{
+    alert("numero mayor es: "+ numero2)
+ }
+ 
+
+
+
