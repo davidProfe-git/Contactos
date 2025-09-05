762202ce2b7249e4a99fd135a8dfe44139f168a3
lucho <luis@gmail.com>
2025-09-05 09:44:43 -0500

    se crea piedra, papel, tijeras

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-let numero1
+
+
+let opciones = prompt ("JUGUEMOS PIEDRA (1) PAPEL (2) O TIJERA (3)")
+let min = 1
+let max = 3
+
+let pc = Math.floor(Math.random() * (max - min + 1) + min) 
+
+
+
+if (pc == 1) {
+   alert ( "pc escoje piedra")
+}
+if (pc == 2) {
+    alert ("pc escoje papel")
+}
+
+if (pc == 3) {
+    alert ("pc escoje tijera")
+}   
+
+if (opciones == pc) {
+    alert ("EMPATE")
+}   else if (opciones == 1 && pc == 2){ 
+    alert ("ganaste")  
+}else if (opciones == 2 && pc == 1){
+    alert ("ganaste")
+}else if (opciones == 3 && pc == 2){
+    alert ("ganaste")   
+}else{
+    alert ("eliga algo hpta")
+}
+
+
+
+
+
+/*let numero1
 let numero2
 
 numero1 = parseInt (prompt("Digite su numero1"))
@@ -16,7 +53,7 @@ if(numero1 > numero2){
 }else{
     alert ("el mayor es el: " + numero2)
 }
-
+*/
 
 
 
