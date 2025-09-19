dccd869b54caf215e61a91e4bf888744342334ad
Sebas <juan.madrigal@pi.edu.co>
2025-09-19 09:27:44 -0500

    primera vercion

--- script.js
+++ script.js
@@ -97,7 +97,7 @@
 //     }
 
 
-fetch("https://randomuser.me/api/?results=5")
+fetch("https://randomuser.me/api/?results=5&nat=mx")
 .then(respuesta => respuesta.json() )
 .then(datos => { 
   const contenedor = document.getElementById("contenedor") 
@@ -107,8 +107,11 @@ fetch("https://randomuser.me/api/?results=5")
         const tarjeta = document.createElement("div")
         tarjeta.className = "tarjeta"
         tarjeta.innerHTML = `
-        <img src="${contacto.picture.lage}"/>
-        <p>${contacto.name.first}</p>
+        <img src="${contacto.picture.large}"/>
+        <p>👤 ${contacto.name.first} ${contacto.name.last}</p>
+        <p>✉ ${contacto.email}</p>
+        <p>📱 ${contacto.phone}</p>
+
         `
         contenedor.appendChild(tarjeta)
   })
