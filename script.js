7a972c5c5c198fa0a479dc3dd69daa70c4b903ea
mateo <mate@gmail.com>
2025-09-23 07:56:17 -0500

    consumi el api

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-fetch("https://randomuser.me/api/")
-.then(respuest => respuesta.json())
-.then(datos =>{
+fetch("https://randomuser.me/api/?results=5&nat=mx")
+
+.then(response => response.json())
+ .then(data => {
+        const contenedor= document.getElementById("contenedor");
+        contenedor.innerHTML = ""
+        data.results.forEach(contacto => {  
+          const  tarjeta = document.createElement("div");
+            tarjeta.className = "tarjeta";
+            tarjeta.innerHTML = `
+            <img src="${contacto.picture.large}"/>
+             <p>👀${contacto.name.first} ${contacto.name.last} </p>
+              <p>📧${contacto.email}</p>  
+              <p>📞${contacto.phone}</p>
+              <p>🖼️${contacto.location.city}, ${contacto.location.country}</p>
+             
+             
+             
+             `;
+         
+           contenedor.appendChild(tarjeta);
+            
+    })
 
 
-    const tarjeta=document.getElementById("contenedor")
-    tarjeta.innerHTML= ""
-    datos.results.forEach(contacto => {
-        const contenido =document.createElement("div")
-        contenido.className("tarjeta")
-        contenido.innerHTML = `
-        <img src="${contacto.picture.large}"/>
-        `
-        tarjeta.appendChild(contenido)
-    }) 
-})
+    })
